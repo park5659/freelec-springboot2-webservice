85747544077ac9b8e9d201b13c9f8971491eb3f4
park5659 <parkch09@hanmail.net>
2020-03-02 03:12:32 +0900

    travisCI

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -7,7 +7,7 @@ source ${ABSDIR}/profile.sh
 function switch_proxy() {
     IDLE_PORT=$(find_idle_port)
 
-    echo "> 전환할Port: $IDLE_PORT"
+    echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
